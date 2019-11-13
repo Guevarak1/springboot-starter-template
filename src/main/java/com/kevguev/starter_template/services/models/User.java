84013d670821b146a617c5d0d3d8f00291eafa3a
kevinguevara <guevarak1@gmail.com>
2019-11-12 20:42:15 -0500

    address models

--- src/main/java/com/kevguev/starter_template/services/models/User.java
+++ src/main/java/com/kevguev/starter_template/services/models/User.java
@@ -9,6 +9,7 @@ public class User {
     private String id;
     private String firstName;
     private String lastName;
+    private Address address;
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
@@ -19,12 +20,13 @@ public class User {
         this.id = userResource.getId();
         this.firstName = userResource.getFirstName();
         this.lastName = userResource.getLastName();
+        this.address = new Address(userResource.getAddress());
     }
 
     public User(UserModel userModel) {
         this.id = userModel.getId();
         this.firstName = userModel.getFirstName();
         this.lastName = userModel.getLastName();
-
+        this.address = new Address(userModel.getAddress());
     }
 }
